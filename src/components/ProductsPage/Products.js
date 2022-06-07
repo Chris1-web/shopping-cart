27cedb9499c4af0f8e07335ed6cfde6b20ccc981
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-06-07 23:40:44 +0100

    store products images

--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -1,6 +1,17 @@
 import React from "react";
+import cheapRoadBicycle from "../images/cheap-Road-Bike.jpg";
+import KidsBicycle from "../images/Kids-Bicycle.jpg";
+import MarketBicycle from "../images/Market-Bicycle.jpg";
+import RenaissanceBicycle from "../images/Renaissance-Street-Bicycle.jpg";
+import SafeRoadBicycle from "../images/safe-road-bicycle.jpg";
+import TouristBicycle from "../images/Tourist-Bicycle.jpg";
+import TwinBicycle from "../images/Twin-Bicycle.jpg";
+import MountainBicycle from "../images/mountain-bicycle.jpg";
+import AdultChoiceBicycle from "../images/AdultChoice.jpg";
 
-const productsImages = [];
+const productsImages = [
+  
+];
 
 const Products = () => {
   return (
