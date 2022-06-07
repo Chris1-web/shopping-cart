cce1d9f03b5b6ee51ee78ebacd20e2ea9e162bdf
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-06-07 20:08:54 +0100

    complete first Nav component test

---
+++ src/components/Navigation/Nav.test.js
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter as Router } from "react-router-dom";
+import Nav from "./Nav";
+
+describe("renders Navigation Component", () => {
+  test("displays navigation tag", () => {
+    render(
+      <Router>
+        <Nav />
+      </Router>
+    );
+    const nav = screen.getByRole("navigation");
+    expect(nav).toBeInTheDocument();
+    expect(nav).toHaveClass("container");
+  });
+  test("displays for items in list", () => {
+    render(
+      <Router>
+        <Nav />
+      </Router>
+    );
+    const listContainer = screen.getByRole("list");
+    const listItem = screen.getAllByRole("listitem");
+    expect(listContainer).toBeInTheDocument();
+    expect(listItem.length).toEqual(4);
+  });
+});
