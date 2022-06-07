a708f6bdf1adde34394a909cf14c20f3e6ec3cc8
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-06-07 17:37:55 +0100

    create expected components folder

---
+++ src/components/Product/Product.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Product = () => {
+  return (
+    <div>
+      <h1>This is the individual Product page</h1>
+    </div>
+  );
+};
+
+export default Product;
---
+++ src/components/ProductsPage/Products.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <h1>This is the general products page</h1>
+    </div>
+  );
+};
+
+export default Products;
