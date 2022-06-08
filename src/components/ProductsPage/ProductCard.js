c41083c546174df99e48c67624cbfc7d70627dc7
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-06-08 23:43:11 +0100

    create product with use effect

--- src/components/ProductsPage/ProductCard.js
+++ src/components/ProductsPage/ProductCard.js
@@ -1,4 +1,10 @@
-export default function ProductCard({ product, showClickedProduct }) {
+export default function ProductCard({ product }) {
+  const increaseQuantity = (e) => {
+    e.preventDefault();
+  };
+  const decreaseQuantity = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="product-card">
       <img
@@ -8,7 +14,16 @@ export default function ProductCard({ product, showClickedProduct }) {
       />
       <h3>{product.name}</h3>
       <p>${product.price}</p>
-      <button>Add To Cart</button>
+      <form>
+        <button className="decrement" onClick={increaseQuantity}>
+          -
+        </button>
+        <input type="number" min="1" value={1} />
+        <button className="increment" onClick={decreaseQuantity}>
+          +
+        </button>
+      </form>
+      <button className="add-to-cart-btn">Add To Cart</button>
     </div>
   );
 }
--- src/components/ProductsPage/Products.js
+++ src/components/ProductsPage/Products.js
@@ -9,23 +9,75 @@ import TouristBicycle from "../images/Tourist-Bicycle.jpg";
 import TwinBicycle from "../images/Twin-Bicycle.jpg";
 import MountainBicycle from "../images/mountainBike.jpg";
 import AdultChoiceBicycle from "../images/AdultChoice.jpg";
+import { useEffect, useState } from "react";
 
 const productsImages = [
-  { name: "Mountain Bicycle", src: MountainBicycle, price: 1000 },
-  { name: "Safe Road Bicycle", src: SafeRoadBicycle, price: 200 },
-  { name: "Renaissance Bicycle", src: RenaissanceBicycle, price: 1000 },
-  { name: "Kid's Bicycle", src: KidsBicycle, price: 200 },
-  { name: "Adult Choice Bicycle", src: AdultChoiceBicycle, price: 500 },
-  { name: "Twin Bicycle", src: TwinBicycle, price: 5000 },
-  { name: "Tourist Bicycle", src: TouristBicycle, price: 300 },
-  { name: "Cheap Road Bicycle", src: cheapRoadBicycle, price: 150 },
-  { name: "Market Bicycle", src: MarketBicycle, price: 500 },
+  {
+    name: "Mountain Bicycle",
+    src: MountainBicycle,
+    price: 1000,
+    quantity: 1,
+  },
+  {
+    name: "Safe Road Bicycle",
+    src: SafeRoadBicycle,
+    price: 200,
+    quantity: 1,
+  },
+  {
+    name: "Renaissance Bicycle",
+    src: RenaissanceBicycle,
+    price: 1000,
+    quantity: 1,
+  },
+  {
+    name: "Kid's Bicycle",
+    src: KidsBicycle,
+    price: 200,
+    quantity: 1,
+  },
+  {
+    name: "Adult Choice Bicycle",
+    src: AdultChoiceBicycle,
+    price: 500,
+    quantity: 1,
+  },
+  {
+    name: "Twin Bicycle",
+    src: TwinBicycle,
+    price: 5000,
+    quantity: 1,
+  },
+  {
+    name: "Tourist Bicycle",
+    src: TouristBicycle,
+    price: 300,
+    quantity: 1,
+  },
+  {
+    name: "Cheap Road Bicycle",
+    src: cheapRoadBicycle,
+    price: 150,
+    quantity: 1,
+  },
+  {
+    name: "Market Bicycle",
+    src: MarketBicycle,
+    price: 500,
+    quantity: 1,
+  },
 ];
 
 export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    setProducts([...productsImages]);
+  }, []);
+
   return (
     <main className="products">
-      {productsImages.map((product, index) => (
+      {products.map((product, index) => (
         <ProductCard product={product} key={index} />
       ))}
     </main>
