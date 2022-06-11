02240870575e79e001435c5257e0bdc76b0f409c
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-06-11 01:41:00 +0100

    remove useless states from ProductCard component

--- src/components/ProductsPage/ProductCard.js
+++ src/components/ProductsPage/ProductCard.js
@@ -1,16 +1,4 @@
-import { useEffect, useState } from "react";
-import Counter from "../Counter";
-// import uniqid from "uniqid";
-// uniqid();
-
 export default function ProductCard({ product, addProductToCart }) {
-  const [inputValue, setInputValue] = useState("");
-
-  useEffect(() => {
-    setInputValue(product.quantity);
-    return () => setInputValue(product.quantity);
-  }, []);
-
   const addToCart = (product) => {
     addProductToCart(product);
   };
@@ -24,11 +12,7 @@ export default function ProductCard({ product, addProductToCart }) {
       />
       <h3>{product.name}</h3>
       <p>${product.price}</p>
-      <button
-        className="add-to-cart-btn"
-        disabled={!inputValue || +inputValue <= 0}
-        onClick={() => addToCart(product)}
-      >
+      <button className="add-to-cart-btn" onClick={() => addToCart(product)}>
         Add To Cart
       </button>
     </article>
