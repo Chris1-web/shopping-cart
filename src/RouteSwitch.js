fd20cd804c52d1366c151fb30b59e907efbf749d
Christianah Adeyemo <christianah123.ac@gmail.com>
2022-09-09 22:24:31 +0100

    minor changes

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -6,7 +6,7 @@ import About from "./components/AboutPage/About";
 
 export default function RouteSwitch() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/shopping-cart">
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
